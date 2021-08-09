da4fb88ed2639692012b7eab8e4ebb1834477d96
Leu Family <romanianfobjr@gmail.com>
2021-08-09 15:26:05 -0500

    premade action connected

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,10 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
+* The button's onClick event is fired, invoking handleAddOne
+* handleAddOne dispatches to addOne
+* in actions/index.js, addOne is invoked which returns the type 'ADD_ONE' and exports it to reducers/index.js
+* reducer in reducers/index.js performs the ADD_ONE case, adding 1 to state.total
 ...
 
 * TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ import './App.css';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
+import { addOne } from './actions'
+
 function App() {
 
   const [state, dispatch] = useReducer(reducer, initialState)
 
   console.log(state)
+
+  const handleAddOne = () => {
+    dispatch(addOne())
+  }
 
   return (
     <div className="App">
@@ -35,7 +41,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handleAddOne}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
